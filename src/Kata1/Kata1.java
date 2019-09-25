5552b39e02ab1f7da9d2a2e65e25fd377098ccf4
CarlosFmmc <CarlosFmmc@10.194.228.241>
2019-09-25 14:02:56 +0100

    Person final?

--- src/Kata1/Kata1.java
+++ src/Kata1/Kata1.java
@@ -1,7 +1,12 @@
 package Kata1;
 
+import java.util.Date;
+
+
 public class Kata1 {
     
-    
+    public static void main(String[] args) {
+        System.out.println(new Person("Jenna Coleman", new Date(86, 4, 27)));
+    }
     
 }
